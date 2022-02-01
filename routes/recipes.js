a062ed9ee431abf359a89922436e0ccfbdcb63d7
Sierra Buckland <Sierra@greatideaz.com>
2022-02-01 17:46:02 -0500

    idk man hopefully this works

--- routes/recipes.js
+++ routes/recipes.js
@@ -70,6 +70,44 @@ router.post('/add', (req, res, next) => {
   }
 });
 
+//get handler for animal edit   
+router.get('/edit/:_id', (req, res, next) => {
+  Recipe.findById(req.params._id, (err, recipe) => {
+      if(err) {
+          console.log(err);
+      }
+      else {
+        res.render('recipes/edit', { 
+          title: 'Edit a Recipe',
+          recipe: recipe,
+      });
+    }   
+  });
+});
+
+//post handler for editing record
+router.post('/edit/:_id', (req, res, next) => {
+  Recipe.findOneAndUpdate({ _id: req.params._id},
+      {
+        author: req.body.author,
+        title: req.body.title,
+        totalTime: req.body.totalTime,
+        cookTime: req.body.cookTime,
+        servings: req.body.servings,
+        ingredients: req.body.ingredients,
+        steps: req.body.steps,
+        rating: req.body.rating,
+        tags: req.body.tags
+  }, (err, updatedRecipe) => {
+      if (err) {
+          console.log(err);
+      }
+      else {
+          res.redirect('/recipes');
+      }
+  });
+});
+
 //DELETE handler for recipe   
 router.get('/delete/:_id', (req, res, next) => {
   Recipe.remove({_id: req.params._id }, (err) => {
