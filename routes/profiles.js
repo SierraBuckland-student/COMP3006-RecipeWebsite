ac85d9330a7e5fb7757e87bcf4e8f58aac09ef53
mylesmegaffin <200431861@student.georgianc.on.ca>
2022-02-06 16:16:26 -0500

    Edit profile by the params in the url

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,10 +1,58 @@
 var express = require('express');
+const user = require('../models/user');
 var router = express.Router();
 const User = require('../models/user'); 
+const passport = require('passport');
+const session = require('express-session');
+
+//checking if the users is logged in
+function IsLoggedIn(req,res,next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 /* GET handler for profile page */
-router.get('/', (req, res, next) => {
+router.get('/:_id', IsLoggedIn, (req, res, next) => {
     res.render('profiles/index', {title: 'Profile', user: req.user});
+    console.log(req.session.passport._id)
 })
+
+/* GET handler for the profile edit page */
+// router.get('/edit', (req, res, next) => {
+//     res.render('profiles/edit', {title: 'Profile', user: req.user});
+// });
+
+router.get('/edit/:_id', IsLoggedIn, (req, res, next) => {
+    
+    User.findById(req.params._id, (err, user) => {
+        
+        if(err) {
+            console.log(err);
+        }
+        else {
+          res.render('profiles/edit', { 
+            title: 'Edit Profile',
+            user: req.user
+        });
+      }   
+    });
+});
+
+router.post('/edit/:_id', IsLoggedIn, (req, res, next) => {
+    console.log(req.params._id)
+    user.findOneAndUpdate({ _id: req.params._id},
+        {
+            username: req.body.username
+        }, (err, updatedRecipe) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect('/recipes');
+        }
+    });
+  });
 
 module.exports = router;
